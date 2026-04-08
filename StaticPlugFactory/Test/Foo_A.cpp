574e9019bdb2f8da49abaa69352e1f0a1fb94d2c
James Edwards <Tavison@yahoo.com>
2026-04-08 16:13:56 -0400

    Making them public.

--- StaticPlugFactory/Test/Foo_A.cpp
+++ StaticPlugFactory/Test/Foo_A.cpp
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2026 James Edwards
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the “Software”), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include "../Factory/FooFactory.h"
 #include "Foo.h"
 
@@ -28,7 +52,7 @@ class Foo_A_Maker : public FooFactory<Foo, const std::string&>
 {
 private:
     // Register this as a type using a static instance of itself.
-    // This is really the only time it is created, which is why the
+    // This is the only time it is created, which is why the
     // constructor is private.
     Foo_A_Maker()
         : FooFactory("Foo_A")
